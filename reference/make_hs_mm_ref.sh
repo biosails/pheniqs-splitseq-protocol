a51cac7b0114e56e751c1039b7cd2f7ccdb75fd7
Lior Galanti <lior.galanti@gmail.com>
2022-01-28 18:50:24 -0500

    Create make_hs_mm_ref.sh

---
+++ reference/make_hs_mm_ref.sh
@@ -0,0 +1,7 @@
+STAR \
+--runThreadN 18 \
+--runMode genomeGenerate \
+--genomeDir /home/lg/split-seq-data/index/gs_mm_hg \
+--genomeFastaFiles  /media/terminus/home/gs/pheniqs/GRCh38_GRCm39.primary_assembly.genome.fa \
+--sjdbGTFfile /media/terminus/home/gs/pheniqs/gencode.combov39_M28.annotation.gtf \
+--sjdbOverhang 65
